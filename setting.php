344fcd706bc99d5ba1f3ca1165343cf1aff6d6b8
Brickme <43787229+Brickme@users.noreply.github.com>
2020-07-10 16:49:53 -0400

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,11 +2,11 @@
 
 class Setting {
 	public static function getChannelAccessToken(){
-		$channelAccessToken = "";
+		$channelAccessToken = "jGFTnK8BQ/kCvxH3BAR353JemKIIydLWnnp4YJ1tnT5maBS2h60Ki+M78GnWI+sAt1Y14Vxjk1MDdDwLuDn0R8N42ATtZq4kEyx0TL3dsB9uwX2Vf4MmYqNy2kUeh/cEphI2rzw8uUAiYEcPFLtyAAdB04t89/1O/w1cDnyilFU=";
 		return $channelAccessToken;
 	}
 	public static function getChannelSecret(){
-		$channelSecret = "";
+		$channelSecret = "eaaececc148e70670a9a64593627d801";
 		return $channelSecret;
 	}
 	public static function getApiReply(){
