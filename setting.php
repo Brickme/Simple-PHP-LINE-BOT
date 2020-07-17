13bd9acfe56fc8809f5c40e2187878bd1887fe16
Brickme <43787229+Brickme@users.noreply.github.com>
2020-07-17 17:52:04 -0400

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,7 +2,7 @@
 
 class Setting {
 	public static function getChannelAccessToken(){
-		$channelAccessToken = "jGFTnK8BQ/kCvxH3BAR353JemKIIydLWnnp4YJ1tnT5maBS2h60Ki+M78GnWI+sAt1Y14Vxjk1MDdDwLuDn0R8N42ATtZq4kEyx0TL3dsB9uwX2Vf4MmYqNy2kUeh/cEphI2rzw8uUAiYEcPFLtyAAdB04t89/1O/w1cDnyilFU=";
+		$channelAccessToken = "AjDDLZgHbhDMmN4ZXuqLo9OhWk9It3eiM1KFJXyYmxJvFcgVmhQ4PODs8bp1rQIzt1Y14Vxjk1MDdDwLuDn0R8N42ATtZq4kEyx0TL3dsB+GLSBuDzULKCnmdeOOksqTaHbZXKx2tUYPP9+YQMWyJgdB04t89/1O/w1cDnyilFU=";
 		return $channelAccessToken;
 	}
 	public static function getChannelSecret(){
